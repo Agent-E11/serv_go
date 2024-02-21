e7fe7bbfd65cb6f9cd9b4a743fc995492ac9ee5a
Eamon Burns <eamon.r.burns@gmail.com>
2024-02-21 02:02:29 -0800

    Add support for glob patterns for files

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
-	"flag"
-	"fmt"
-	"net/http"
-	"text/template"
+    "flag"
+    "fmt" // TODO: Change all debug `fmt`s prints to `log`
+    "net/http"
+    "path/filepath"
+    "strings"
+    "text/template"
 )
 
 func main() {
@@ -25,17 +27,34 @@ func main() {
         fmt.Println("Not permissive")
     }
 
-    // Set the file to the first parameter (if specified)
+    // Set the file to the first parameter
+    // if specified, and if it's not a glob pattern. Else,
+    // set to index.html
     var file string
     if flag.Arg(0) == "" {
+        file = "index.html"
+    } else if strings.Contains(flag.Arg(0), "*") ||
+        strings.Contains(flag.Arg(0), "?") ||
+        strings.Contains(flag.Arg(0), "[") {
         file = "index.html"
     } else {
         file = flag.Arg(0)
     }
     fmt.Printf("The root file is `%s`\n", file)
 
+    // Create the default route
     http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
         fmt.Println("Serving /")
+        fmt.Printf("Value of r.URL.Path: %v\n", r.URL.Path)
+
+        // If the path is not the root, return a 404
+        if r.URL.Path != "/" {
+            fmt.Println("Before not found error")
+            http.NotFound(w, r)
+            fmt.Println("After not found error")
+            return
+        }
+
         tmpl, err := template.ParseFiles(file)
         if err != nil {
             fmt.Printf("The file %s does not exist\n", file)
@@ -44,19 +63,34 @@ func main() {
 
         err = tmpl.Execute(w, nil)
         if err != nil {
-            fmt.Printf("Error executing template: %v", err)
+            fmt.Printf("Error executing template: %v\n", err)
         }
     })
 
-    // TODO: Search for files using filepath.Glob,
-    // And then make handlers for every file found
-    for _, f := range flag.Args() {
-        http.HandleFunc(
-            fmt.Sprintf("/%s", f),
-            generateHandler(f),
-        )
+    // Search for files using a glob pattern
+    // and then make handlers for every file found
+    for _, descriptor := range flag.Args() {
+
+        files, err := filepath.Glob(descriptor)
+        if err != nil {
+            fmt.Printf("Error reading glob: %v\n", err)
+            continue
+        }
+        if files == nil {
+            fmt.Println("No files described by", descriptor)
+            continue
+        }
+
+        for _, f := range files {
+            // TODO: Only make handler if file is not a directory
+            http.HandleFunc(
+                fmt.Sprintf("/%s", f),
+                generateHandler(f),
+            )
+        }
     }
 
+    // Create portString
     var portString string
     if *external {
         portString = fmt.Sprintf("0.0.0.0:%d", *port)
@@ -64,6 +98,7 @@ func main() {
         portString = fmt.Sprintf(":%d", *port)
     }
 
+    // Listen for http requests
     fmt.Println("Listening on port", portString)
     http.ListenAndServe(portString, nil)
 }
@@ -83,7 +118,7 @@ func generateHandler(file string) http.HandlerFunc {
 
         err = tmpl.Execute(w, nil)
         if err != nil {
-            fmt.Printf("Error executing template: %v", err)
+            fmt.Printf("Error executing template: %v\n", err)
         }
     }
 }
